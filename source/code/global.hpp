099916fc4c38bd6bc972e51d5388b528bc4223aa
sago007 <sago007@9d7177f8-192b-0410-8f35-a16a89829b06>
2011-06-10 17:39:47 +0000

    Missing file
    git-svn-id: https://blockattack.svn.sourceforge.net/svnroot/blockattack/trunk@120 9d7177f8-192b-0410-8f35-a16a89829b06

---
+++ source/code/global.hpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   global.hpp
+ * Author: poul
+ *
+ * Created on 5. juni 2011, 18:26
+ */
+
+#ifndef _GLOBAL_HPP
+#define	_GLOBAL_HPP
+
+#include "CppSdl/CppSdlImageHolder.hpp"
+#include "Libs/NFont.h"
+
+void MainMenu();
+void ResetFullscreen();
+
+extern CppSdl::CppSdlImageHolder menuMarked;
+extern CppSdl::CppSdlImageHolder menuUnmarked;
+extern NFont nf_scoreboard_font;
+extern bool MusicEnabled;			//true if background music is enabled
+extern bool SoundEnabled;			//true if sound effects is enabled
+extern bool bFullscreen;			//true if game is running fullscreen
+extern char player1name[30];
+extern char player2name[30];
+extern SDL_Surface *screen;        //The whole screen;
+
+#endif	/* _GLOBAL_HPP */
+
