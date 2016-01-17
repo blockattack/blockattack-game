7c10bd5192f8d9ebd30ff35a24a36bcf292e123f
sago007 <poul@poulsander.com>
2016-01-17 17:35:18 +0100

    Some changes to the pack script to make travis build the man page too

--- packdata.sh
+++ packdata.sh
@@ -3,5 +3,7 @@ set -e
 zip -9rj Game/blockattack.data source/AUTH
 cd Game/data
 zip -9r ../blockattack.data * -x \*svn*
+cd ../../man
+docbook-to-man blockattack.sgml|gzip -9 > blockattack.6.gz
 
 exit 0
