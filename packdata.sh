1b9ec1076748d43b3fb3ad347147066fd0ef3eef
sago007 <poul@poulsander.com>
2016-03-15 21:26:58 +0100

    Updated the packaged man-file

--- packdata.sh
+++ packdata.sh
@@ -4,7 +4,7 @@ zip -9rj Game/blockattack.data source/AUTH
 cd Game/data
 zip -9r ../blockattack.data * -x \*svn*
 cd ../../man
-docbook-to-man blockattack.sgml|gzip -9 > blockattack.6.gz
+gzip -9 -c blockattack.man > blockattack.6.gz
 cd ../source/misc/translation/
 ./build_mo_all.sh
 
