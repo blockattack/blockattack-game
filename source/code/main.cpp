8dc4a9d6c67fcb081855b64644cb6c4238c4c7c1
sago007 <poul@poulsander.com>
2016-03-16 20:02:16 +0100

    Updated the help and thereby the man page

--- source/code/main.cpp
+++ source/code/main.cpp
@@ -1493,7 +1493,7 @@ int main(int argc, char* argv[]) {
 			//NULL on Windows
 			commandname = argv[0];
 		}
-		boost::program_options::options_description desc("Allowed options", consoleWidth);
+		boost::program_options::options_description desc("Options", consoleWidth);
 		desc.add_options()
 		("help,h", "Displays this message")
 		("version", "Display the version information")
@@ -1529,18 +1529,22 @@ int main(int argc, char* argv[]) {
 		}
 		if (vm.count("help")) {
 			cout << SPrintStringF("Block Attack - Rise of the blocks %s\n\n"
+									"Block Attack - Rise of the Blocks is a puzzle/blockfall game inspired by Tetris Attack for the SNES.\n\n"
 			                      "%s\n\n", VERSION_NUMBER, "www.blockattack.net");
 			cout << "Usage: "<< commandname << " [OPTION]..." << endl;
 			cout << desc << endl;
 			cout << "Examples:" << endl;
-			cout << "\tblockattack\t Start the game normally" << endl;
+			cout << "\tblockattack          \tStart the game normally" << endl;
 			cout << "\tblockattack --nosound\tStart the game without sound. Can be used if sound problems prevents the game from starting" << endl;
+			cout << endl;
+			cout << "Report bugs to the issue tracker here: <https://github.com/blockattack/blockattack-game/issues>" << endl;
 			return 0;
 		}
 		if (vm.count("version")) {
-			cout << "blockattack : Block Attack - Rise of the Blocks " << VERSION_NUMBER << endl;
+			cout << "blockattack " << VERSION_NUMBER << endl;
+			cout << endl;
 			cout << "Copyright (C) 2005-2016 Poul Sander" << endl;
-			cout << "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>" << endl;
+			cout << "License GPLv2+: GNU GPL version 2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html> or later <http://gnu.org/licenses/gpl.html>" << endl;
 			cout << "This is free software: you are free to change and redistribute it." << endl;
 			cout << "There is NO WARRANTY, to the extent permitted by law." << endl;
 			return 0;
