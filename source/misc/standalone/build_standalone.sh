ab86a00f303be6853db62ab5264e6189520e2f45
sago007 <poul@poulsander.com>
2017-08-31 21:08:27 +0200

    Better standalone support

--- source/misc/standalone/build_standalone.sh
+++ source/misc/standalone/build_standalone.sh
@@ -2,12 +2,14 @@
 set -e
 
 rm -rf staging
-mkdir staging
+mkdir -p staging/x86_64
+cp blockattack_standalone_launcher staging/blockattack
+chmod +x staging/blockattack
 
 cd ../../..
 
-docker build -f source/misc/docker/Dockerfile.Ubuntu14.04build . -t blockattack_test
+docker build -f source/misc/docker/Dockerfile.Ubuntu14.04build_Standalone . -t blockattack_test
 
 echo Copying to: $(pwd)/source/misc/standalone/staging/
 
-docker run -it --rm -v $(pwd)/source/misc/standalone/staging/:/output blockattack_test /bin/bash -c "cp /staging/blockattack-game/Game/blockattack /output/ && cp /staging/blockattack-game/Game/blockattack.data /output/"
+docker run -it --rm -v $(pwd)/source/misc/standalone/staging/:/output blockattack_test /bin/bash -c "cp /staging/blockattack-game/Game/blockattack /output/x86_64/ && cp /staging/blockattack-game/Game/blockattack.data /output/"
