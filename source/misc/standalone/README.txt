195987ded3e55c6344ea82492174a57707265ad5
sago007 <poul@poulsander.com>
2017-09-02 14:26:04 +0200

    Can now build a Linux standalone package

---
+++ source/misc/standalone/README.txt
@@ -0,0 +1,10 @@
+Block Attack - Rise of the Blocks - Linux build
+
+This is the standalone version. It is precompiled and includes the necessary libraries to run.
+
+Both a 32 bit and 64 bit version is provided.
+It has been tested on Ubuntu 14.04 and Fedora 22
+Some systems might require the game to be launched from the command line.
+PulseAudio is required for sound.
+
+Check www.blockattack.net for more info.
---
+++ source/misc/standalone/compile_requirements.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -e
+set -x
+
+mkdir -p /staging/deps && cd /staging/deps && curl https://libsdl.org/release/SDL2-2.0.5.tar.gz | tar -zx && cd SDL2-2.0.5 && ls -lrt
+cd /staging/deps/SDL2-2.0.5 && ./configure --enable-shared --enable-static && make && make install
+
+#https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
+mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz | tar -zx && cd SDL2_image-2.0.1 && ls -lrt
+cd /staging/deps/SDL2_image-2.0.1 && ./configure --enable-shared --enable-static && make && make install
+
+#https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz
+mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz | tar -zx && cd SDL2_mixer-2.0.1 && ls -lrt
+cd /staging/deps/SDL2_mixer-2.0.1 && ./configure --enable-shared --enable-static && make && make install
+
+#https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
+mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz | tar -zx && cd SDL2_ttf-2.0.14 && ls -lrt
+cd /staging/deps/SDL2_ttf-2.0.14 && ./configure --enable-shared --enable-static && make && make install
+mkdir -p /staging/blockattack-game
+
+mkdir -p /staging/deps && cd /staging/deps && curl http://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2 | tar -jx && cd physfs-2.0.3 && ls -lrt
+cd /staging/deps/physfs-2.0.3 && cmake . && make && make install
