7ebf97853b7d05bfa1bad9d4c5ba61c3a55ae6e2
sago007 <poul@poulsander.com>
2017-09-02 12:51:57 +0200

    Added a 32 bit version for standalone

--- source/misc/standalone/build_standalone.sh
+++ source/misc/standalone/build_standalone.sh
@@ -10,6 +10,7 @@ ARCHIVENAME=$1
 
 rm -rf staging
 mkdir -p staging/$ARCHIVENAME/x86_64
+mkdir -p staging/$ARCHIVENAME/x86
 mkdir -p staging/$ARCHIVENAME/docs
 cp blockattack_standalone_launcher staging/$ARCHIVENAME/blockattack
 cp README.txt staging/$ARCHIVENAME/README
@@ -37,3 +38,15 @@ cp /staging/blockattack-game/COPYING /output/ && \
 cp /staging/blockattack-game/man/blockattack.man /output/docs/ && \
 cp /staging/blockattack-game/README.md /output/docs/README_ORG.md && \
 chown -R 1000 /output"
+
+docker build -f source/misc/docker/Dockerfile.Ubuntu14.04build_Standalone32 . -t blockattack_test
+
+docker run -it --rm -v $(pwd)/source/misc/standalone/staging/$ARCHIVENAME/:/output blockattack_test /bin/bash -c "cp /staging/blockattack-game/Game/blockattack /output/x86/ && \
+cp /usr/local/lib/libSDL2-2.0.so.0 /output/x86/ && \
+cp /usr/local/lib/libphysfs.so.1  /output/x86/ && \
+cp /usr/local/lib/libSDL2_mixer-2.0.so.0  /output/x86/ && \
+cp /usr/local/lib/libSDL2_ttf-2.0.so.0  /output/x86/ && \
+cp /usr/local/lib/libSDL2_image-2.0.so.0  /output/x86/"
+
+cd source/misc/standalone/staging/
+tar -cvjSf "$ARCHIVENAME.tar.bz2" "$ARCHIVENAME"
