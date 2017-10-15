32da6f0c2b142a2916e405941842c07fc029310d
sago007 <poul@poulsander.com>
2017-10-15 12:07:43 +0200

    Updated the standaline build to SDL2-2.0.6

--- source/misc/standalone/compile_requirements.sh
+++ source/misc/standalone/compile_requirements.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-mkdir -p /staging/deps && cd /staging/deps && curl https://libsdl.org/release/SDL2-2.0.5.tar.gz | tar -zx && cd SDL2-2.0.5 && ls -lrt
-cd /staging/deps/SDL2-2.0.5 && ./configure --enable-shared --enable-static && make && make install
+mkdir -p /staging/deps && cd /staging/deps && curl https://libsdl.org/release/SDL2-2.0.6.tar.gz | tar -zx && cd SDL2-2.0.6 && ls -lrt
+cd /staging/deps/SDL2-2.0.6 && ./configure --enable-shared --enable-static && make && make install
 
 #https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
 mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz | tar -zx && cd SDL2_image-2.0.1 && ls -lrt
