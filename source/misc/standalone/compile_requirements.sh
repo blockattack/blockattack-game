dfbb311ea45d08b8b3bb935d0fd83dd087d42f27
Poul Sander <poul@poulsander.com>
2019-01-10 18:46:15 +0100

    Update SDL components in standalone build

--- source/misc/standalone/compile_requirements.sh
+++ source/misc/standalone/compile_requirements.sh
@@ -2,16 +2,16 @@
 set -e
 set -x
 
-mkdir -p /staging/deps && cd /staging/deps && curl https://libsdl.org/release/SDL2-2.0.6.tar.gz | tar -zx && cd SDL2-2.0.6 && ls -lrt
-cd /staging/deps/SDL2-2.0.6 && ./configure --enable-shared --enable-static && make && make install
+mkdir -p /staging/deps && cd /staging/deps && curl https://libsdl.org/release/SDL2-2.0.9.tar.gz | tar -zx && cd SDL2-2.0.9 && ls -lrt
+cd /staging/deps/SDL2-2.0.9 && ./configure --enable-shared --enable-static && make && make install
 
-#https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
-mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz | tar -zx && cd SDL2_image-2.0.1 && ls -lrt
-cd /staging/deps/SDL2_image-2.0.1 && ./configure --enable-shared --enable-static && make && make install
+#https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.4.tar.gz
+mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.4.tar.gz | tar -zx && cd SDL2_image-2.0.4 && ls -lrt
+cd /staging/deps/SDL2_image-2.0.4 && ./configure --enable-shared --enable-static && make && make install
 
-#https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz
-mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz | tar -zx && cd SDL2_mixer-2.0.1 && ls -lrt
-cd /staging/deps/SDL2_mixer-2.0.1 && ./configure --enable-shared --enable-static && make && make install
+#https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz
+mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz | tar -zx && cd SDL2_mixer-2.0.4 && ls -lrt
+cd /staging/deps/SDL2_mixer-2.0.4 && ./configure --enable-shared --enable-static && make && make install
 
 #https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
 mkdir -p /staging/deps && cd /staging/deps && curl https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz | tar -zx && cd SDL2_ttf-2.0.14 && ls -lrt
