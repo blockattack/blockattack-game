988137eed75c6bd5bbeda099767840dad4e6168c
Poul Sander <poul@poulsander.com>
2021-06-08 23:28:06 +0200

    Clean up standalone and check that it works with the new cmake

--- source/misc/standalone/build_standalone.sh
+++ source/misc/standalone/build_standalone.sh
@@ -10,7 +10,6 @@ ARCHIVENAME=$1
 
 rm -rf staging
 mkdir -p staging/$ARCHIVENAME/x86_64
-mkdir -p staging/$ARCHIVENAME/x86
 mkdir -p staging/$ARCHIVENAME/docs
 cp blockattack_standalone_launcher staging/$ARCHIVENAME/blockattack
 cp README.txt staging/$ARCHIVENAME/README
@@ -40,43 +39,3 @@ cp /staging/blockattack-game/README.md /output/docs/README_ORG.md && \
 chown -R 1000 /output"
 
 exit 0
-# Following is from the old 32-bit build
-
-scp source/misc/standalone/compile_requirements.sh blockattack_build:/
-scp source/misc/standalone/install_requirements.sh blockattack_build:/
-ssh blockattack_build /install_requirements.sh
-ssh blockattack_build /compile_requirements.sh
-ssh blockattack_build rm -rf /staging/blockattack-game
-scp -r . blockattack_build:/staging/blockattack-game
-ssh blockattack_build rm -f /staging/blockattack-game/CMakeCache.txt
-
-ssh blockattack_build "BLOCKATTACK_VERSION=2.2.0-SNAPSHOT && cd /staging/blockattack-game && \
-./packdata.sh && \
-cmake -D Boost_USE_STATIC_LIBS=ON -D INSTALL_DATA_DIR=. -D CMAKE_INSTALL_PREFIX=. -D STANDALONE=1 . && make clean && make"
-
-OUTPUT=$(pwd)/source/misc/standalone/staging/$ARCHIVENAME/
-
-scp blockattack_build:/staging/blockattack-game/Game/blockattack $OUTPUT/x86/ && \
-scp blockattack_build:/usr/local/lib/libSDL2-2.0.so.0 $OUTPUT/x86/ && \
-scp blockattack_build:/usr/local/lib/libphysfs.so.1  $OUTPUT/x86/ && \
-scp blockattack_build:/usr/local/lib/libSDL2_mixer-2.0.so.0  $OUTPUT/x86/ && \
-scp blockattack_build:/usr/local/lib/libSDL2_ttf-2.0.so.0  $OUTPUT/x86/ && \
-scp blockattack_build:/usr/lib/i386-linux-gnu/libfreetype.so.6 $OUTPUT/x86/ && \
-scp blockattack_build:/lib/i386-linux-gnu/libpng12.so.0 $OUTPUT/x86/ && \
-scp blockattack_build:/usr/local/lib/libSDL2_image-2.0.so.0  $OUTPUT/x86/
-
-exit 0
-
-docker build -f source/misc/docker/Dockerfile.Ubuntu12.04build_Standalone32 . -t blockattack_test
-
-docker run -it --rm -v $(pwd)/source/misc/standalone/staging/$ARCHIVENAME/:/output blockattack_test /bin/bash -c "cp /staging/blockattack-game/Game/blockattack /output/x86/ && \
-cp /usr/local/lib/libSDL2-2.0.so.0 /output/x86/ && \
-cp /usr/local/lib/libphysfs.so.1  /output/x86/ && \
-cp /usr/local/lib/libSDL2_mixer-2.0.so.0  /output/x86/ && \
-cp /usr/local/lib/libSDL2_ttf-2.0.so.0  /output/x86/ && \
-cp /usr/lib/i386-linux-gnu/libfreetype.so.6 /output/x86/ && \
-cp /lib/i386-linux-gnu/libpng12.so.0 /output/x86/ && \
-cp /usr/local/lib/libSDL2_image-2.0.so.0  /output/x86/"
-
-cd source/misc/standalone/staging/
-tar -cvjSf "$ARCHIVENAME.tar.bz2" "$ARCHIVENAME"
