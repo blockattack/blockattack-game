5f4823d5429cf97473de74ae1e0e8e1e03717bd5
Poul Sander <poul@poulsander.com>
2021-11-02 18:54:55 +0100

    New packdata to make the modified date of the zip archive more predictable.

--- packdata.sh
+++ packdata.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-zip -9jX  Game/blockattack.data source/AUTH
+zip -9ojX  Game/blockattack.data source/AUTH
 pushd Game/data
-zip -9X $(find ../blockattack.data ./* | sort) -x \*svn*
+zip -9oX $(find ../blockattack.data ./* | sort) -x \*svn*
 popd
 pushd Game/mods/1.3.0.bricks_unpacked.data/
-zip -9X ../1.3.0.bricks.data $(find ./* | sort)
+zip -9oX ../1.3.0.bricks.data $(find ./* | sort)
 popd
 pushd man
 gzip -9n -c blockattack.man > blockattack.6.gz
