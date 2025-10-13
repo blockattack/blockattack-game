0918c503b3b56fbe3ecf8386e3ef7d658bd928cd
Poul Sander <poul@unity3d.com>
2025-10-13 13:12:12 +0200

    Slight updates to the docker images now that C++17 is mandatory

--- source/misc/standalone/compile_requirements.sh
+++ source/misc/standalone/compile_requirements.sh
@@ -4,8 +4,8 @@ set -x
 set -u
 set -o pipefail
 
-mkdir -p /staging/deps && cd /staging/deps && curl -L https://github.com/libsdl-org/SDL/releases/download/release-2.30.2/SDL2-2.30.2.tar.gz | tar -zx && cd SDL2-2.30.2 && ls -lrt
-cd /staging/deps/SDL2-2.30.2 && ./configure --enable-shared --enable-static && make && make install
+mkdir -p /staging/deps && cd /staging/deps && curl -L https://github.com/libsdl-org/SDL/releases/download/release-2.32.10/SDL2-2.32.10.tar.gz | tar -zx && cd SDL2-2.32.10 && ls -lrt
+cd /staging/deps/SDL2-2.32.10 && ./configure --enable-shared --enable-static && make && make install
 
 #https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/SDL2_image-2.6.2.tar.gz
 mkdir -p /staging/deps && cd /staging/deps && curl -L https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.2/SDL2_image-2.8.2.tar.gz | tar -zx && cd SDL2_image-2.8.2 && ls -lrt
@@ -27,9 +27,9 @@ cd /staging/deps/physfs-3.0.2 && cmake . && make && make install
 cd ~
 mkdir -p Downloads
 cd Downloads
-curl https://files.poulsander.com/~poul19/public_files/boost_1_65_1.tar.bz2 -O
-tar xvfj boost_1_65_1.tar.bz2
-pushd boost_1_65_1
+curl https://archives.boost.io/release/1.89.0/source/boost_1_89_0.tar.bz2 -O
+tar xvfj boost_1_89_0.tar.bz2
+pushd boost_1_89_0
 ./bootstrap.sh --with-libraries=program_options
 ./b2 install -j 2 --prefix=/usr link=static
 popd
